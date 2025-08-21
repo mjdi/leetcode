eb9e6e7ca9b8d06689dc83443b30bce445ab31a4
Marcus Diemand <mjdiemand@gmail.com>
2025-08-21 16:34:19 -0400

    added tests from 236, 437, 1372 leetcode problems

---
+++ leetcode75/_1448/tests/test_1448.py
@@ -0,0 +1,15 @@
+import leetcode75.dshelper.treenode as tn
+from leetcode75._1448.solution import Solution as sol
+
+class TestClass:
+    def test_one(self):
+        root = tn.create_binary_tree([[3,1,4,3,None,1,5]])
+        assert 4 == sol.goodNodes(root)
+
+    def test_two(self):
+        root = tn.create_binary_tree([[3,3,None,4,2]])
+        assert 3 == sol.goodNodes(root)
+
+    def test_three(self):
+        root = tn.create_binary_tree([[1]])
+        assert 1 == sol.goodNodes(root)
---
+++ leetcode75/_236/solution.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/?envType=study-plan-v2&envId=leetcode-75
+from leetcode75.dshelper.treenode import TreeNode
+
+class Solution:
+    @staticmethod
+    def lowestCommonAncestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        return TreeNode()
