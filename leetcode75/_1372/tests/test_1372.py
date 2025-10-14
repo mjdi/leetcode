956a0b82d37cbb15f1d9b9ea81891094d5566f2e
Marcus Diemand <mjdiemand@gmail.com>
2025-10-14 18:57:14 -0400

    fixed create_binary_tree(), solved 37/75

--- leetcode75/_1372/tests/test_1372.py
+++ leetcode75/_1372/tests/test_1372.py
@@ -4,13 +4,13 @@ from leetcode75._1372.solution import Solution as sol
 class TestClass:
     def test_one(self):
         root = tn.create_binary_tree([1,None,1,1,1,None,None,1,1,None,1,None,None,None,1])
-        assert 3 == sol().longestZigZig(root)
+        assert 3 == sol().longestZigZag(root)
         
     def test_two(self):
         root = tn.create_binary_tree([1,1,1,None,1,None,None,1,1,None,1])
-        assert 4 == sol().longestZigZig(root)
+        assert 4 == sol().longestZigZag(root)
         
     def test_three(self):
         root = tn.create_binary_tree([1])
-        assert 1 == sol().longestZigZig(root)
+        assert 0 == sol().longestZigZag(root)
     
