d636382fb3f790495975f1e06abc12b8e123e806
Marcus Diemand <mjdiemand@gmail.com>
2025-10-27 16:46:00 -0400

    53/75

--- leetcode75/_374/solution.py
+++ leetcode75/_374/solution.py
@@ -1,6 +1,37 @@
+# https://leetcode.com/problems/guess-number-higher-or-lower/description/?envType=study-plan-v2&envId=leetcode-75
 from typing import Optional
-                
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Binary_Search_Guesser:
+    def __init__(self, actual:int ):
+        self.actual = actual
+        
+    def guess(self, gguess):
+        if gguess == self.actual:
+            return 0
+        elif gguess > self.actual:
+            return -1
+        else: 
+            return 1
+  
 class Solution:
-    def () -> :
-        return
+    def guessNumber(self, n: int, actual:int) -> int:
+        g = Binary_Search_Guesser(actual)
+        if g.guess(n) == 0:
+            return n
+        si = [1,n] # search interval
+        curr_guess = (si[1] + si[0]) // 2
+        while g.guess(curr_guess) != 0:
+            if g.guess(curr_guess) == 1: # guess is lower than actual
+                si = [curr_guess, si[1]] # search top half
+            else: # guess is higher than actual
+                si = [si[0], curr_guess] # search bottom half
+            curr_guess = (si[1] + si[0]) // 2
+
+        return curr_guess
 
--- leetcode75/_374/tests/test_374.py
+++ leetcode75/_374/tests/test_374.py
@@ -2,11 +2,16 @@ from leetcode75._374.solution import Solution as sol
                 
 class TestClass:
     def test_one(self):
-        assert == sol().
+        assert 6== sol().guessNumber(10,6)
 
     def test_two(self):
-        assert == sol().
+        assert 1 == sol().guessNumber(1,1)
 
     def test_three(self):
-        assert == sol().
+        assert 1 == sol().guessNumber(2,1)
 
+    def test_four(self):
+        assert 2 == sol().guessNumber(2,2)
+
+    def test_five(self):
+        assert 4 == sol().guessNumber(5,4)
