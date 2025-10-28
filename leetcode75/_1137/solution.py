b3bdd9143530176b02bba9cd957a4242a6ec5be8
Marcus Diemand <mjdiemand@gmail.com>
2025-10-28 16:10:30 -0400

    66/75

--- leetcode75/_1137/solution.py
+++ leetcode75/_1137/solution.py
@@ -1,6 +1,11 @@
+# https://leetcode.com/problems/n-th-tribonacci-number/description/?envType=study-plan-v2&envId=leetcode-75
 from typing import Optional
                 
 class Solution:
-    def () -> :
-        return
-
+    def tribonacci(self, n: int) -> int:
+        t = [0,1,1]
+        if n < len(t):
+            return t[n]
+        for i in range(3,n+1):
+            t.append(t[i-1] + t[i-2] + t[i-3])
+        return t[n]
--- leetcode75/_1137/tests/test_1137.py
+++ leetcode75/_1137/tests/test_1137.py
@@ -2,11 +2,11 @@ from leetcode75._1137.solution import Solution as sol
                 
 class TestClass:
     def test_one(self):
-        assert == sol().
+        assert 4 == sol().tribonacci(4)
 
     def test_two(self):
-        assert == sol().
+        assert 1389537 == sol().tribonacci(25)
 
     def test_three(self):
-        assert == sol().
+        assert 2082876103 == sol().tribonacci(37)
 
